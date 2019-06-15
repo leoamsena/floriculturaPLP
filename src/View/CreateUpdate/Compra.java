aadf790ca75dc0d448d7d51f86044d6213d5dfd7
leoamsena <leonardo123.amorim@gmail.com>
2019-06-14 22:10:34 -0300

    Forms nos .java

--- src/View/CreateUpdate/Compra.java
+++ src/View/CreateUpdate/Compra.java
@@ -25,8 +25,9 @@ public class Compra implements ActionListener {
     private Model.Compra compra;
 
 
-    private Map<Model.Flor,Integer> listaTemporaria;
-    private void refreshList(){
+    private Map<Model.Flor, Integer> listaTemporaria;
+
+    private void refreshList() {
         this.listaFlores.removeAll();
         int cont = 0;
         for (Model.Flor o : this.listaTemporaria.keySet()) {
@@ -53,6 +54,7 @@ public class Compra implements ActionListener {
         this.listaFlores.revalidate();
         this.listaFlores.repaint();
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.checkValor) {
@@ -75,42 +77,42 @@ public class Compra implements ActionListener {
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(this.panel, ex.getMessage());
             }
-        }else if(e.getSource() instanceof JButtonWithObject){ // é para diminuir ou excluir da lista de flores
+        } else if (e.getSource() instanceof JButtonWithObject) { // é para diminuir ou excluir da lista de flores
             JButtonWithObject bt = (JButtonWithObject) e.getSource();
             int novoValor = this.listaTemporaria.get(bt.getObject());
-            if(bt.getName().equals("excluir") || novoValor <= 1 ){
+            if (bt.getName().equals("excluir") || novoValor <= 1) {
                 this.listaTemporaria.remove(bt.getObject());
-            }else if(bt.getName().equals("diminuir")){
-                 novoValor -= 1;
+            } else if (bt.getName().equals("diminuir")) {
+                novoValor -= 1;
                 this.listaTemporaria.replace((Model.Flor) bt.getObject(), novoValor);
             }
             refreshList();
-        }else if(e.getSource() == this.enviarButton){
+        } else if (e.getSource() == this.enviarButton) {
             float valor = -1;
-            if(!this.checkValor.isSelected()) {
+            if (!this.checkValor.isSelected()) {
                 valor = Float.valueOf(this.valorTotal.getText().substring(3).trim());
             }
-            if(this.compra != null){ //editando
-                this.compra.setCliente((Model.Cliente)this.cliente.getSelectedItem());
+            if (this.compra != null) { //editando
+                this.compra.setCliente((Model.Cliente) this.cliente.getSelectedItem());
                 this.compra.setFlores(this.listaTemporaria);
                 this.compra.setFuncionario(MainController.getUsuarioLogado());
-                if(valor != -1)
+                if (valor != -1)
                     this.compra.setValorTotal(valor);
                 else
                     this.compra.calculaValorAutomatico();
-            }else{ //criando
-                if(valor == -1) {
+            } else { //criando
+                if (valor == -1) {
                     MainController.addToArrayList(new Model.Compra((Model.Cliente) this.cliente.getSelectedItem(), this.listaTemporaria, MainController.getUsuarioLogado()));
-                }else {
+                } else {
                     MainController.addToArrayList(new Model.Compra(valor, (Model.Cliente) this.cliente.getSelectedItem(), this.listaTemporaria, MainController.getUsuarioLogado()));
                 }
             }
-            JOptionPane.showMessageDialog(this.panel,"Criado/editado com sucesso!!!");
+            JOptionPane.showMessageDialog(this.panel, "Criado/editado com sucesso!!!");
             (HomeController.genericCRUDList).refreshPainelComponents();
             Component component = (Component) e.getSource();
             ((JDialog) SwingUtilities.getRoot(component)).dispose();
 
-        }else if(e.getSource() == this.cancelarButton){
+        } else if (e.getSource() == this.cancelarButton) {
             Component component = (Component) e.getSource();
             ((JDialog) SwingUtilities.getRoot(component)).dispose();
         }
@@ -123,20 +125,24 @@ public class Compra implements ActionListener {
     public void setPanel(JPanel panel) {
         this.panel = panel;
     }
-    public Compra(){
+
+    public Compra() {
         visivel = true;
+        $$$setupUI$$$();
         this.valorTotal.setVisible(visivel);
-        this.listaTemporaria = new HashMap<Model.Flor,Integer>();
+        this.listaTemporaria = new HashMap<Model.Flor, Integer>();
         this.checkValor.addActionListener(this);
         this.add.addActionListener(this);
         this.enviarButton.addActionListener(this);
         this.cancelarButton.addActionListener(this);
     }
-    public Compra(Model.Compra compra){
+
+    public Compra(Model.Compra compra) {
         this.compra = compra;
         visivel = true;
+        $$$setupUI$$$();
         this.valorTotal.setVisible(visivel);
-        this.listaTemporaria = new HashMap<Model.Flor,Integer>();
+        this.listaTemporaria = new HashMap<Model.Flor, Integer>();
         this.checkValor.addActionListener(this);
         this.add.addActionListener(this);
         this.enviarButton.addActionListener(this);
@@ -153,5 +159,54 @@ public class Compra implements ActionListener {
         this.cliente = new JComboBox(MainController.getClientes().toArray());
         this.listaFlores = new JPanel();
         this.listaFlores.setLayout(new GridBagLayout());
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        createUIComponents();
+        panel = new JPanel();
+        panel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(7, 3, new Insets(0, 0, 0, 0), -1, -1));
+        final JLabel label1 = new JLabel();
+        label1.setText("Venda");
+        panel.add(label1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 3, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Valor total");
+        panel.add(label2, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        checkValor = new JCheckBox();
+        checkValor.setText("Cálculo automático");
+        panel.add(checkValor, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label3 = new JLabel();
+        label3.setText("Flores");
+        panel.add(label3, new com.intellij.uiDesigner.core.GridConstraints(4, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        panel.add(floresCombo, new com.intellij.uiDesigner.core.GridConstraints(4, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        add = new JButton();
+        add.setText("Adicionar flor na lista");
+        panel.add(add, new com.intellij.uiDesigner.core.GridConstraints(4, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        panel.add(listaFlores, new com.intellij.uiDesigner.core.GridConstraints(5, 0, 1, 3, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        valorTotal = new JFormattedTextField();
+        panel.add(valorTotal, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        enviarButton = new JButton();
+        enviarButton.setText("Enviar");
+        panel.add(enviarButton, new com.intellij.uiDesigner.core.GridConstraints(6, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        cancelarButton = new JButton();
+        cancelarButton.setText("Cancelar");
+        panel.add(cancelarButton, new com.intellij.uiDesigner.core.GridConstraints(6, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label4 = new JLabel();
+        label4.setText("Cliente");
+        panel.add(label4, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        panel.add(cliente, new com.intellij.uiDesigner.core.GridConstraints(3, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panel;
     }
 }
